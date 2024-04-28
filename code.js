88342feda4e23c34b96680fbb291e909eadf35e2
wudora <126846421+wudora@users.noreply.github.com>
2024-04-28 18:02:57 -0400

    s3f

--- code.js
+++ code.js
@@ -1,38 +1,215 @@
 const boardHeight = 20;
 const boardWidth = 25;
 const gameBoard = document.getElementById('game-board');
+let board = [];
+let carX = 0;
+let carY = 0;
 const car = document.getElementById('car');
-const size = 10;
+const projectTitle = document.getElementById('projectTitle');
+const ogTitle = projectTitle.innerText;
+const projectText = document.getElementById('projectText');
+const ogText = projectText.innerText;
+const go = document.getElementById('go');
 
 function createBoard() {
     for (let i = 0; i < boardHeight; i++) {
+        board[i] = [];
         for (let j = 0; j < boardWidth; j++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            gameBoard.appendChild(cell);
+            if (i == 0 && j == 0) {
+                board[0][0] = car;
+            }
+            else if (i == 3 && j == 5) {
+                const s1a1 = document.createElement('img');
+                s1a1.id = 's1a1';
+                s1a1.src = 'images/cone.png';
+                s1a1.classList.add('project');
+                gameBoard.appendChild(s1a1);
+                board[i][j] = s1a1;
+            }
+            else if (i == 3 && j == 12) {
+                const s1a2 = document.createElement('img');
+                s1a2.id = 's1a2';
+                s1a2.src = 'images/cone.png';
+                s1a2.classList.add('project');
+                gameBoard.appendChild(s1a2);
+                board[i][j] = s1a2;
+            }
+            else if (i == 1 && j == 18) {
+                const s1f = document.createElement('img');
+                s1f.id = 's1f';
+                s1f.src = 'images/cone.png';
+                s1f.classList.add('project');
+                gameBoard.appendChild(s1f);
+                board[i][j] = s1f;
+            }
+            else if (i == 5 && j == 22) {
+                const s2a1 = document.createElement('img');
+                s2a1.id = 's2a1';
+                s2a1.src = 'images/cone.png';
+                s2a1.classList.add('project');
+                gameBoard.appendChild(s2a1);
+                board[i][j] = s2a1;
+            }
+            else if (i == 10 && j == 15) {
+                const s2a2 = document.createElement('img');
+                s2a2.id = 's2a2';
+                s2a2.src = 'images/cone.png';
+                s2a2.classList.add('project');
+                gameBoard.appendChild(s2a2);
+                board[i][j] = s2a2;
+            }
+            else if (i == 8 && j == 7) {
+                const s2f = document.createElement('img');
+                s2f.id = 's2f';
+                s2f.src = 'images/cone.png';
+                s2f.classList.add('project');
+                gameBoard.appendChild(s2f);
+                board[i][j] = s2f;
+            }
+            else if (i == 12 && j == 3) {
+                const s3a1 = document.createElement('img');
+                s3a1.id = 's3a1';
+                s3a1.src = 'images/cone.png';
+                s3a1.classList.add('project');
+                gameBoard.appendChild(s3a1);
+                board[i][j] = s3a1;
+            }
+            else if (i == 17 && j == 6) {
+                const s3a2 = document.createElement('img');
+                s3a2.id = 's3a2';
+                s3a2.src = 'images/cone.png';
+                s3a2.classList.add('project');
+                gameBoard.appendChild(s3a2);
+                board[i][j] = s3a2;
+            }
+            else if (i == 15 && j == 18) {
+                const s3f = document.createElement('img');
+                s3f.id = 's3f';
+                s3f.src = 'images/cone.png';
+                s3f.classList.add('project');
+                gameBoard.appendChild(s3f);
+                board[i][j] = s3f;
+            }
+            else {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                gameBoard.appendChild(cell);
+                board[i][j] = cell;
+            }
         }
     }
-    car.style.top = '5vh';
-    car.style.left = '5vw';
+}
+
+function displayProject(project){
+    const id = project.id;
+    if (car.style.transform == 'rotate(90deg)' || car.style.transform == 'rotate(0deg)') {
+        project.style.transform = 'rotate(-70deg)';
+    }
+    else {
+        project.style.transform = 'rotate(70deg)';
+    }
+    go.style.visibility = 'visible';
+    if (id == "s1a1") {
+        projectTitle.innerText = 'Section 1 Assignment 1';
+        projectText.innerText = "welhbvwlr";
+        go.href = "https://codepen.io/Dora-Wu-the-sans/pen/mdoxyaZ?editors=1100";
+    }
+    else if (id == "s1a2") {
+        projectTitle.innerText = 'Section 1 Assignment 2';
+        projectText.innerText = "welhbvwlr";
+        go.href = "https://codepen.io/Dora-Wu-the-sans/pen/MWxrxyg?editors=1100";
+    }
+    else if (id == "s1f") {
+        projectTitle.innerText = 'Section 1 Final Project';
+        projectText.innerText = "welhbvwlr";
+        go.href = "decision/index.html";
+    }
+    else if (id == "s2a1") {
+        projectTitle.innerText = 'Section 2 Assignment 1';
+        projectText.innerText = "welhbvwlr";
+        go.href = "decision/index.html";
+    }
+    else if (id == "s2a2") {
+        projectTitle.innerText = 'Section 2 Assignment 2';
+        projectText.innerText = "welhbvwlr";
+        go.href = "smiski/index.html";
+    }
+    else if (id == "s2f") {
+        projectTitle.innerText = 'Section 2 Final Project';
+        projectText.innerText = "welhbvwlr";
+        go.href = "https://editor.p5js.org/wudora/sketches/NEC-FDB6b";
+    }
+    else if (id == "s3a1") {
+        projectTitle.innerText = 'Section 3 Assignment 1';
+        projectText.innerText = "welhbvwlr";
+        go.href = "closet/index.html";
+    }
+    else if (id == "s3a2") {
+        projectTitle.innerText = 'Section 3 Assignment 2';
+        projectText.innerText = "welhbvwlr";
+        go.href = "s3a2/index.html";
+    }
+    else if (id == "s3f") {
+        projectTitle.innerText = 'Section 3 Final Project';
+        projectText.innerText = "welhbvwlr";
+        go.href = "";
+    }
+}
+
+function moveCar(row, col) {
+    const temp = board[row][col];
+    if (temp.classList.contains('project')) {
+        displayProject(temp);
+    }
+    else {
+        projectTitle.innerText = ogTitle;
+        projectText.innerText = ogText;
+        go.style.visibility = 'hidden';
+        const index1 = Array.from(gameBoard.children).indexOf(car);
+        const index2 = Array.from(gameBoard.children).indexOf(temp);
+        gameBoard.removeChild(car);
+        gameBoard.insertBefore(car, gameBoard.children[index2]);
+        gameBoard.removeChild(temp);
+        gameBoard.insertBefore(temp, gameBoard.children[index1]);
+        board[row][col] = car;
+        board[carY][carX] = temp;
+        carY = row;
+        carX = col;
+    }
 }
 
 function handleArrowKeys(event) {
     const arrowKey = event.key;
     switch (arrowKey) {
         case 'ArrowUp':
-            car.style.top = (car.offsetTop - size) + 'px';
+            if(carY > 0) {
+                moveCar(carY - 1, carX);
+                car.style.transform = 'rotate(180deg)';
+            }
             break;
         case 'ArrowDown':
-            car.style.top = (car.offsetTop + size) + 'px';
+            if(carY < boardHeight - 1) {
+                moveCar(carY + 1, carX);
+                car.style.transform = 'rotate(0deg)';
+            }
             break;
         case 'ArrowLeft':
-            car.style.left = (car.offsetLeft - size) + 'px';
+            if(carX > 0) {
+                moveCar(carY, carX - 1);
+                car.style.transform = 'rotate(90deg)';
+            }
             break;
         case 'ArrowRight':
-            car.style.left = (car.offsetLeft + size) + 'px';
+            if(carX < boardWidth - 1) {
+                moveCar(carY, carX + 1);
+                car.style.transform = 'rotate(270deg)';
+            }
             break;
     }
 }
 
 createBoard();
 document.addEventListener('keydown', handleArrowKeys);
+moveCar(1, 0);
+moveCar(2, 0);
+moveCar(3, 0);
---
+++ s3f/sketch.js
@@ -0,0 +1,73 @@
+let width = window.innerWidth;
+let height = window.innerHeight;
+const size = 50;
+let x = size / 2;
+let y = size / 2;
+let colors = [];
+let show = false;
+
+const r1 = new Set([4, 10, 11, 14, 15, 18, 19, 20]);
+const r2 = new Set([5, 12, 13, 16, 17, 21, 22, 23]);
+const g1 = new Set([2, 6, 7, 10, 12, 18, 21, 22]);
+const g2 = new Set([3, 8, 9, 11, 13, 20, 20, 23]);
+const b1 = new Set([0, 6, 8, 14, 16, 18, 19, 21, 23]);
+const b2 = new Set([1, 7, 9, 15, 17, 20, 22]);
+
+
+function setup() {
+    background(255);
+}
+
+function draw() {
+    if (show == true) {
+        while (y < height) {
+            let rand = int(random(0, colors.length));
+            fill(colors[rand][0], colors[rand][1], colors[rand][2]);
+            ellipse(x, y, size);
+            x += size;
+            if (x > width) {
+                x = size / 2;
+                y += size;
+            }
+        }
+    }
+}
+
+
+function generate() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    createCanvas(width, height);
+    colors.length = 0;
+    noStroke();
+    const text = document.getElementById("inputValue").value;
+    const letters = text.split('');
+    for (let i = 0; i < letters.length; i++) {
+        let val = letters[i].charCodeAt() - 65;
+        colors[i] = [];
+        colors[i][0] = 3 * val;
+        colors[i][1] = 3 * val;
+        colors[i][2] = 3 * val;
+        if (r1.has(val % 24)) {
+            colors[i][0] = 255;
+        }
+        if (g1.has(val % 24)) {
+            colors[i][1] = 255;
+        }
+        if (b1.has(val % 24)) {
+            colors[i][2] = 255;
+        }
+        if (r2.has(val % 24)) {
+            colors[i][0] = (255 + colors[i][0]) / 2;
+        }
+        if (g2.has(val % 24)) {
+            colors[i][1] = (255 + colors[i][1]) / 2;
+        }
+        if (b2.has(val % 24)) {
+            colors[i][2] = (255 + colors[i][2]) / 2;
+        }
+    }
+    show = true;
+    x = size / 2;
+    y = size / 2;
+}
